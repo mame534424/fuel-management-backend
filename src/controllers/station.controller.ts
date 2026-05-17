c2f46b49ef004d006cd15e2925b38a5dc4edc1ae
Mohammed Abrar <mame.534424@gmail.com>
2026-05-17 23:51:56 +0300

    feat: implement getNearbyStations function for querying nearby stations

--- src/controllers/station.controller.ts
+++ src/controllers/station.controller.ts
@@ -1,6 +1,8 @@
-import {request, response} from "express";
+import {Request,Response} from "express";
 import { db } from "../config/db";
 import { stations } from "../db/schema";
+import { sql } from "drizzle-orm";
+
 import {AuthenticatedRequest} from "../middleware/auth.middleware";
 
 export async function createStation(req:AuthenticatedRequest,res:any){
@@ -18,6 +20,33 @@ export async function createStation(req:AuthenticatedRequest,res:any){
         res.status(201).json({message:"Station created successfully",station:station[0]});
     } catch (error) {
         console.error("Error in createStation:", error);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+export async function getNearbyStations(req:Request,res:any){
+    try {
+        const {latitude,longitude}=req.query;
+        if(!latitude || !longitude){
+            return res.status(400).json({message:"Latitude and Longitude are required"});
+        }
+        const nearbyStations=await db.select().from(stations).where(sql`
+        (
+        POWER(${stations.latitude} - ${Number(latitude)}, 2)
+        +
+        POWER(${stations.longitude} - ${Number(longitude)}, 2)
+        ) < 0.01
+        `).orderBy(sql`
+        (
+        POWER(${stations.latitude} - ${Number(latitude)}, 2)
+        +
+        POWER(${stations.longitude} - ${Number(longitude)}, 2)
+        )
+        `);
+        
+        res.status(200).json(nearbyStations);
+    } catch (error) {
+        console.error("Error in getNearbyStations:", error);
         res.status(500).json({message:"Internal Server Error"});
     }
 }
