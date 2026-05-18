dc95246459e3ed43add6d1d63078612771d7ad9e
Mohammed Abrar <mame.534424@gmail.com>
2026-05-18 10:35:10 +0300

    feat: add getNearbyStations route to station router

--- src/routes/station.routes.ts
+++ src/routes/station.routes.ts
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import { createStation } from "../controllers/station.controller";
+import { createStation, getNearbyStations } from "../controllers/station.controller";
 import { authorize, protect } from "../middleware/auth.middleware";
 const stationRouter=Router();
 
 stationRouter.post("/create",protect,authorize("admin"),createStation);
+stationRouter.get("/nearby",getNearbyStations);
 
 export default stationRouter;
